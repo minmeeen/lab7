d347704816c02d1cb118a1e234c128edc6be4ef5
minmeeen <sirapat.mp@gmail.com>
2021-01-04 04:07:10 +0700

    lab7_3_edit

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,11 +1,50 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	int age;
+	double height;
+	double prop;
+	string status;
+	string x= "UNFRIEND";
+	string y= "FRIEND";
+	string z = "MARRIED";
+	string ons = "ONE-NIGHT-STAND";
+	string bf = "BEST FRIEND";
+	
+	cout << "Enter your age: ";
+	cin >> age;
+	if(age<=20){
+		cout << "Enter your height: ";
+		cin >> height;
+		if(height<160){
+			status = x;
+		}else if(height<175){
+			status = y;
+		}else{
+			cout << "Enter your property: ";
+			cin>> prop;
+			if(prop>69000000){
+				status = z;
+			}else{
+				status = ons;
+			}
+		}
+	}else if(age<30){
+		cout << "Enter your property: ";
+		cin >> prop;
+		if(prop>10000000){
+			status = bf;
+		}else{
+			status =  x;
+		}
+	}else{
+		status = x;
+	}
+	
+	cout<< "Your status = " << status;
+
+
+	return 0;
+}
